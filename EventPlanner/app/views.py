5366bf8d802e9b018fcae56020d7457c24b723fe
alvinsgithub <alvin.y@berkeley.edu>
2014-07-19 20:16:31 -0700

    New Template #2

--- EventPlanner/app/views.py
+++ EventPlanner/app/views.py
@@ -5,7 +5,7 @@ Created on Jul 2, 2014
 '''
 from app import app, lm, sqldb
 from models import User, Event, Team, Member
-from forms import LoginForm, RegisterForm, EventForm, TeamForm
+from forms import EventForm
 from flask.ext.login import login_user, logout_user, current_user, login_required
 from flask import render_template, flash, redirect, session, url_for, request, g, jsonify
 from datetime import datetime, date, time, timedelta
@@ -17,7 +17,10 @@ def hello():
 
 @app.route('/createEvent', methods = ['GET', 'POST'])
 def createEvent():
-    return render_template("createEvent.html")
+    form = EventForm()
+    if form.validate_on_submit():
+        flash('Event Created.')
+    return render_template("createEvent.html", form=form)
 
 @app.route('/suggestions/<string:type>', methods = ['GET', 'POST'])
 def getSuggestions(type):
