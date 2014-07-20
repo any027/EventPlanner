5209083571a7f6cd891b6450ceae88ed71c31ce6
alvinsgithub <alvin.y@berkeley.edu>
2014-07-19 22:02:42 -0700

    SQL connection

--- EventPlanner/app/forms.py
+++ EventPlanner/app/forms.py
@@ -7,7 +7,7 @@ from flask.ext.wtf import Form
 from wtforms import TextField, PasswordField, DateTimeField, IntegerField, validators
 from wtforms.validators import Required, Email
 from wtforms.validators import ValidationError
-from models import User
+#from models import User
 
 class EventForm(Form):
     name = TextField('name', [Required('Name is required')])
